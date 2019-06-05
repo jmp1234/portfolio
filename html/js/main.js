e0189a15ac5fbf54aeea87201d40bfc2f5b18a8f
jmp1234 <johnnypham944@yahoo.com>
2019-06-05 16:35:02 +0200

    sdf

--- html/js/main.js
+++ html/js/main.js
@@ -29,14 +29,26 @@
 /*========Window Load Function========*/
 $(window).on("load", function() {
 
-    $('body').on('touchstart', (e) => {
-      // $('body').css('background-color', 'red');
-      e.preventDefault()
-    })
-    $('body').on('click', (e) => {
-      $('body').css('background-color', 'red');
-      // e.preventDefault()
-    })
+    // $('body').on('touchstart', (e) => {
+    //   // $('body').css('background-color', 'red');
+    //   e.preventDefault()
+    // })
+    // $('body').on('click', (e) => {
+    //   $('body').css('background-color', 'red');
+    //   // e.preventDefault()
+    // })
+    // 1. Get a target element that you want to persist scrolling for (such as a modal/lightbox/flyout/nav).
+    const targetElement = document.querySelector(".modal-open");
+
+// 2. ...in some event handler after showing the target element...disable body scroll
+  bodyScrollLock.disableBodyScroll(targetElement);
+
+// 3. ...in some event handler after hiding the target element...
+  bodyScrollLock.enableBodyScroll(targetElement);
+
+// 4. Useful if we have called disableBodyScroll for multiple target elements,
+// and we just want a kill-switch to undo all that.
+  bodyScrollLock.clearAllBodyScrollLocks();
 
     /*========Preloader Setup========*/
     setTimeout(function(){
